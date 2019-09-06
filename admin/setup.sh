e3460dd54a536ca3274f6adcf5ff952b62445dc8
Saiful Habib <sangpejuang32@gmail.com>
2019-09-06 14:21:23 +0700

    setting awal development aplikasi

--- admin/setup.sh
+++ admin/setup.sh
@@ -2,5 +2,5 @@
 
 # Install a pre-commit hook that
 # automatically runs phpcs to fix styles
-cp admin/pre-commit .git/hooks/pre-commit
-chmod +x .git/hooks/pre-commit
+#cp admin/pre-commit .git/hooks/pre-commit
+#chmod +x .git/hooks/pre-commit
